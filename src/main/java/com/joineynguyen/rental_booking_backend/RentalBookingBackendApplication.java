568f2f51a5be235879bed8f63bc945593b409d56
Joiney <joineyn901@gmail.com>
2025-08-27 01:40:23 -0500

    initial spring boot setup

---
+++ src/main/java/com/joineynguyen/rental_booking_backend/RentalBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.joineynguyen.rental_booking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalBookingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joineynguyen/rental_booking_backend/RentalBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joineynguyen.rental_booking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
